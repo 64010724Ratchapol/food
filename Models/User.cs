0b02eda939cd0f57c29ad44056cdeaa44a200bbc
64010724Ratchapol <88644530+64010724Ratchapol@users.noreply.github.com>
2023-04-27 07:43:42 +0700

    Create User.cs

---
+++ Models/User.cs
@@ -0,0 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace foodde.Models
+{
+    public class User
+    {
+        
+        public string Name { get; set; }
+        [Key]
+        public int Id { get; set; } 
+        public string Store { get; set; }
+        public string Menu { get; set; }
+        public string Address { get; set; }
+        public int Number { get; set; }
+    }
+}
